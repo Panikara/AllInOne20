40277662ff94c38dc68b0c99981646c126da8145
spuser <spuser@VSPT2>
2020-01-15 04:39:11 -0800

    Insert

---
+++ AllInOne20/Controllers/RegisterController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AllInOne20.Controllers
+{
+    public class RegisterController : Controller
+    {
+        RegisterDbEntities dbContext = new RegisterDbEntities();
+        // GET: Register
+        public ActionResult Index(RegisterTable regDetails)
+        {
+            dbContext.RegisterTables.Add(regDetails);
+            dbContext.SaveChanges();
+            return View();
+        }
+    }
+}
---
+++ AllInOne20/PTEGRE.js
@@ -0,0 +1,15 @@
+﻿/// <reference path="C:\Users\spuser.VTSLINDIA\Desktop\sampath\AllInOne20\AllInOne20\scripts/angular.js" />
+var app = angular.module("MypageInfo", []);
+app.controller("MypageController", function ($scope,$http) {
+   
+    $scope.Insert = function (S) {
+        alert("Hi");
+        $http({
+            method: "POST",
+            url: "/Register/Index",
+            data:S,
+            dataType: 'json',
+            headers: { "Content-Type": "application/json" }
+        })
+    }
+})
