83cbee603e7d7e3332490996272a0237caac0c09
spuser <spuser@VSPT2>
2020-01-17 05:09:29 -0800

    Insert 17/1/20

--- AllInOne20/Controllers/RegisterController.cs
+++ AllInOne20/Controllers/RegisterController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,8 +11,29 @@ namespace AllInOne20.Controllers
     {
         RegisterDbEntities dbContext = new RegisterDbEntities();
         // GET: Register
+   
         public ActionResult Index(RegisterTable regDetails)
         {
+            string path = Server.MapPath("~/Uploads/");
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            if (Request.Files.Count > 0)
+            {
+                foreach (string key in Request.Files)
+                {
+
+               
+                        HttpPostedFileBase postedFile = Request.Files[0];
+                        postedFile.SaveAs(path + postedFile.FileName);
+
+                        string url = "Uploads/" + postedFile.FileName;
+
+                  
+                }
+            }
+          
             dbContext.RegisterTables.Add(regDetails);
             dbContext.SaveChanges();
             return View();
--- AllInOne20/PTEGRE.js
+++ AllInOne20/PTEGRE.js
@@ -1,9 +1,14 @@
 ﻿/// <reference path="C:\Users\spuser.VTSLINDIA\Desktop\sampath\AllInOne20\AllInOne20\scripts/angular.js" />
 var app = angular.module("MypageInfo", []);
-app.controller("MypageController", function ($scope,$http) {
+app.controller("MypageController", function ($scope, $http, $window) {
+    
    
+    
     $scope.Insert = function (S) {
+       
+     
         alert("Hi");
+       
         $http({
             method: "POST",
             url: "/Register/Index",
@@ -11,5 +16,16 @@ app.controller("MypageController", function ($scope,$http) {
             dataType: 'json',
             headers: { "Content-Type": "application/json" }
         })
+    }
+    $scope.SelectedFiles = "";
+    $scope.SelectFile = function (e) {
+        alert("Files");
+        $window.alert(e.target.files[0]);
+        $scope.SelectedFiles = e.target.files[0];
+        console.log($scope.SelectedFiles);
+        $scope.S = {
+            IELTSPTEImage: $scope.SelectedFiles
+        }
+        
     }
 })
