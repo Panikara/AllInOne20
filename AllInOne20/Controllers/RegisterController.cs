f6f13267fa6d66485a748b434f6112b335b47ea2
spuser <spuser@VSPT2>
2020-01-18 05:00:54 -0800

    17/1/20

--- AllInOne20/Controllers/RegisterController.cs
+++ AllInOne20/Controllers/RegisterController.cs
@@ -9,10 +9,19 @@ namespace AllInOne20.Controllers
 {
     public class RegisterController : Controller
     {
+
         RegisterDbEntities dbContext = new RegisterDbEntities();
         // GET: Register
    
         public ActionResult Index(RegisterTable regDetails)
+        {
+           
+            dbContext.RegisterTables.Add(regDetails);
+            dbContext.SaveChanges();
+            return View();
+        }
+
+        public ActionResult ImagOne()
         {
             string path = Server.MapPath("~/Uploads/");
             if (!Directory.Exists(path))
@@ -21,22 +30,74 @@ namespace AllInOne20.Controllers
             }
             if (Request.Files.Count > 0)
             {
-                foreach (string key in Request.Files)
-                {
+                //foreach (string key in Request.Files)
+               // {
 
-               
-                        HttpPostedFileBase postedFile = Request.Files[0];
-                        postedFile.SaveAs(path + postedFile.FileName);
 
-                        string url = "Uploads/" + postedFile.FileName;
+                    HttpPostedFileBase postedFile = Request.Files[0];
+                    postedFile.SaveAs(path + postedFile.FileName);
 
-                  
-                }
+                    string url1 = "Uploads/" + postedFile.FileName;
+
+                return Json(url1, JsonRequestBehavior.AllowGet);
+                // }
             }
-          
-            dbContext.RegisterTables.Add(regDetails);
-            dbContext.SaveChanges();
             return View();
+            
+        }
+
+        public ActionResult ImagSecond()
+        {
+            string path = Server.MapPath("~/Uploads/");
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            if (Request.Files.Count > 0)
+            {
+                //foreach (string key in Request.Files)
+                // {
+
+
+                HttpPostedFileBase postedFile = Request.Files[0];
+                postedFile.SaveAs(path + postedFile.FileName);
+
+                string url2 = "Uploads/" + postedFile.FileName;
+
+                return Json(url2, JsonRequestBehavior.AllowGet);
+                // }
+            }
+            return View();
+        }
+
+        public ActionResult ImagThree()
+        {
+            string path = Server.MapPath("~/Uploads/");
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            if (Request.Files.Count > 0)
+            {
+                //foreach (string key in Request.Files)
+                // {
+
+
+                HttpPostedFileBase postedFile = Request.Files[0];
+                postedFile.SaveAs(path + postedFile.FileName);
+
+                string url3 = "Uploads/" + postedFile.FileName;
+
+                return Json(url3, JsonRequestBehavior.AllowGet);
+                // }
+            }
+            return View();
+        }
+
+        public ActionResult Show()
+        {
+            List<RegisterTable> getDetails = dbContext.RegisterTables.ToList();
+            return Json(getDetails, JsonRequestBehavior.AllowGet);
         }
     }
 }
--- AllInOne20/PTEGRE.js
+++ AllInOne20/PTEGRE.js
@@ -1,31 +1,127 @@
 ﻿/// <reference path="C:\Users\spuser.VTSLINDIA\Desktop\sampath\AllInOne20\AllInOne20\scripts/angular.js" />
-var app = angular.module("MypageInfo", []);
-app.controller("MypageController", function ($scope, $http, $window) {
-    
-   
-    
+var app = angular.module("MypageInfo", ['ngFileUpload']);
+app.controller("MypageController", function ($scope, $http, $window, Upload) {
+
+
+
     $scope.Insert = function (S) {
-       
-     
+
+
         alert("Hi");
-       
+
         $http({
             method: "POST",
             url: "/Register/Index",
-            data:S,
+            data: S,
             dataType: 'json',
             headers: { "Content-Type": "application/json" }
         })
     }
-    $scope.SelectedFiles = "";
-    $scope.SelectFile = function (e) {
-        alert("Files");
-        $window.alert(e.target.files[0]);
-        $scope.SelectedFiles = e.target.files[0];
-        console.log($scope.SelectedFiles);
-        $scope.S = {
-            IELTSPTEImage: $scope.SelectedFiles
+
+    $scope.UploadFiles = function (files) {
+
+
+
+        $scope.SelectedFiles = files;
+
+
+        if ($scope.SelectedFiles && $scope.SelectedFiles.length) {
+            Upload.upload({
+                url: '/Register/ImagOne',
+                data: {
+                    files: $scope.SelectedFiles
+                }
+
+
+            }).
+            then(function (response) {
+             
+                $scope.S = {
+                    IELTSPTEImage: response.data
+                }
+             
+                console.log($scope.S.IELTSPTEImage);
+
+            })
+
         }
+
+    }
+
+
+
+
+
+
+
+
+    $scope.UploadFiles1 = function (files) {
+      
+      
+    
+        $scope.SelectedFiles1 = files;
+  
+
+        if ($scope.SelectedFiles1 && $scope.SelectedFiles1.length) {
+            Upload.upload({
+                url: '/Register/ImagSecond',
+                data: {
+                    PGorGraduateImage: $scope.SelectedFiles1
+                }
+            }).
+            then(function (response) {
+             
+                $scope.S = {
+                    PGorGraduateImage: response.data
+                }
+              
+                console.log($scope.S.PGorGraduateImage);
+
+            })
+        }
+    }
+
+    $scope.UploadFiles2 = function (files) {
+    
+  
+        $scope.SelectedFiles2 = files;
+        
+
+        if ($scope.SelectedFiles2 && $scope.SelectedFiles2.length) {
+            Upload.upload({
+                url: '/Register/ImagThree',
+                data: {
+                    files: $scope.SelectedFiles2
+                }
+            }).
+             then(function (response) {
+              
+                 $scope.S = {
+                     event: response.data
+                 }
+               
+                 console.log($scope.S.SSCINTERImage);
+
+             })
+        }
+    }
+
+    $scope.Show = function () {
+        alert("Show")
+        $http.get("/Register/Show").then(function (response) {
+            $scope.GetDetails = response.data;
+        })
+
         
     }
+
+    $(document).ready(function () {
+        $("#show").click(function () {
+            $("#mytable").DataTable({
+               
+            });
+        })
+       
+    });
+   
 })
